ff72df96ce9465783da63a9ef38394b720318618
Rus <rus85aliev@gmail.com>
2022-07-04 16:59:53 +0300

    redux practice

---
+++ src/components/heroesList/HeroesList.js
@@ -0,0 +1,96 @@
+import { useHttp } from '../../hooks/http.hook';
+import { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { createSelector } from 'reselect'
+
+import { fetchData, heroDeleted } from '../../actions';
+import HeroesListItem from "../heroesListItem/HeroesListItem";
+import Spinner from '../spinner/Spinner';
+
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import './heroesList.scss'
+
+
+const HeroesList = () => {
+
+    const filteredHeroesState = createSelector(
+        state => state.filters.activeFilter,
+        state => state.heroes.heroes,
+        (filter, heroes) => {
+            if (filter === 'all') {
+                return heroes
+            } else {
+                return heroes.filter(item => item.element === filter)
+            }
+        }
+    )
+    
+    // const filteredHeroes = useSelector(state => {
+    //     if (state.filters.activeFilter === 'all') {
+    //         console.log('render')
+    //         return state.heroes.heroes
+    //     } else {
+    //         return state.heroes.heroes.filter(item => item.element === state.filters.activeFilter)
+    //     }
+    // })
+
+    const filteredHeroes = useSelector(filteredHeroesState)
+    const heroesLoadingStatus = useSelector(state => state.heroes.heroesLoadingStatus);
+    const dispatch = useDispatch();
+    const { request } = useHttp();
+
+
+    useEffect(() => {
+        dispatch(fetchData(request));
+    }, []);
+
+    // const onDelete = useCallback((id) => {
+    //     request(`http://localhost:3001/heroes/${id}`, 'DELETE')
+    //     .then(dispatch(heroDeleted(id)))
+    //     .catch(err => console.log(err))
+    // }, [request, dispatch]) 
+
+    const onDelete = useCallback((id) => {
+        dispatch(heroDeleted(id))
+    }, [dispatch])
+
+    if (heroesLoadingStatus === "loading") {
+        return <Spinner />
+    } else if (heroesLoadingStatus === "error") {
+        return <h5 className="text-center mt-5">Ошибка загрузки</h5>
+    }
+
+    const renderHeroesList = (arr) => {
+        if (arr.length === 0) {
+            return (
+                <CSSTransition
+                    timeout={500}
+                    classNames='hero'
+                >
+                    <h5 className="text-center mt-5">Героев пока нет</h5>
+                </CSSTransition>
+            )
+        }
+
+        return arr.map(({ id, ...props }) => {
+            return (
+                <CSSTransition
+                    key={id}
+                    timeout={500}
+                    classNames='hero'
+                >
+                    <HeroesListItem onDelete={() => onDelete(id)} {...props} />
+                </CSSTransition>
+            )
+        })
+    }
+
+    const elements = renderHeroesList(filteredHeroes);
+    return (
+        <TransitionGroup component='ul'>
+            {elements}
+        </TransitionGroup>
+    )
+}
+
+export default HeroesList;
